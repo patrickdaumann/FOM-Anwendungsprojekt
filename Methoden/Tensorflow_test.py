87034875343f3e48fb33416d575e2d0f106e41e8
Marian <101745797+KesperM@users.noreply.github.com>
2024-01-14 22:10:20 +0100

    added testfile

---
+++ Methoden/Tensorflow_test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 14 22:03:32 2024
+
+@author: MK
+"""
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import pandas as pd
+
+model = keras.models.load_model("/Users/MK/Documents/GitHub/FOM-Anwendungsprojekt/Models/c_rt-g_s_o-as_n_100epochs_001.h5")
+testpath = "/Users/MK/Documents/GitHub/FOM-Anwendungsprojekt/Data/Output/Airbnb_Prices_V1.0_Test.csv"
+testdata = pd.read_csv(testpath, sep=';', decimal='.')
+x_t = testdata[['cleanliness_rating', 'guest_satisfaction_overall', 'AttractionScore_Norm']].values
+y_t = testdata[['realSum']].values
+predictions = model.predict(x_t)
+print(predictions)
