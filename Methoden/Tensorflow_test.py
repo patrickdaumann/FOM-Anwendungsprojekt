c78383fed9c64fceb8761ea6a4ef5c59e7889002
Marian <101745797+KesperM@users.noreply.github.com>
2024-01-16 21:48:11 +0100

    Test

--- Methoden/Tensorflow_test.py
+++ Methoden/Tensorflow_test.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import pandas as pd
-
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 model = keras.models.load_model("/Users/MK/Documents/GitHub/FOM-Anwendungsprojekt/Models/c_rt-g_s_o-as_n_100epochs_001.h5")
 testpath = "/Users/MK/Documents/GitHub/FOM-Anwendungsprojekt/Data/Output/Airbnb_Prices_V1.0_Test.csv"
 testdata = pd.read_csv(testpath, sep=';', decimal='.')
