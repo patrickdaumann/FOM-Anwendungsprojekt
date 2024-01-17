791a83e5113a95b15499b35c79bc4f83af6270b0
Marian <101745797+KesperM@users.noreply.github.com>
2024-01-17 16:44:07 +0100

    Create Tensorflow_04_linux.py

---
+++ Methoden/Tensorflow_04_linux.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 17 16:28:00 2024
+@author: kesper
+"""
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import pandas as pd
+ 
+# Beispiel-Daten: Ein eindimensionales Array von Features und zugehörige Zielvariablen
+#X = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
+#y = np.array([2.0, 4.0, 6.0, 8.0, 10.0])
+
+# Annahme: Ihre Daten sind in einer Datei mit dem Namen "data.csv"
+path = "/mnt/c/Users/Admin/FOM-Anwendungsprojekt/Data/Output/Airbnb_Prices_V1.0_Train.csv"
+
+data = pd.read_csv(path, sep=';', decimal='.')
+
+x = data[['attr_index_norm', 'rest_index_norm', 'person_capacity', 'AttractionScore_Norm']].values
+y = data[['realSum_Normalized']].values
+ 
+# Erstellen eines einfachen neuronalen Netzwerks
+model = keras.Sequential([
+    layers.Dense(64, activation='relu', input_shape=(3,)),  # Eingabe ist 3-dimensional
+    layers.Dense(32, activation='relu'),
+    layers.Dense(1)  # Die Ausgabe ist 1-dimensional
+])
+ 
+# Kompilieren des Modells
+model.compile(optimizer='adam', loss='mean_squared_error')  # Für Regressionsaufgaben
+ 
+# Training des Modells
+model.fit(x, y, epochs=1000)  # Wir verwenden die gleichen Daten für Training und Test
+ 
+# Evaluieren des Modells (optional)
+loss = model.evaluate(x, y)
+print(f'Loss auf den Trainingsdaten: {loss:.4f}')
+ 
+# Vorhersagen mit dem trainierten Modell
+predictions = model.predict(x)
+print(predictions)
+model.save("/mnt/c/Users/Admin/FOM-Anwendungsprojekt/Models/train-csv-a_i_n-r_i_n-p_c-AS_N-100epochs_001.h5")
+ 
+# Die Vorhersagen sollten nun nahe an den Zielvariablen liegen, da es sich um ein einfaches Beispiel handelt.
+
+
+#testpath = "/Users/MK/Documents/GitHub/FOM-Anwendungsprojekt/Data/Output/Airbnb_Prices_V1.0_Test.csv"
+#testdata = pd.read_csv(testpath, sep=';', decimal='.')
+#x_t = testdata[['cleanliness_rating', 'guest_satisfaction_overall', 'AttractionScore_Norm']].values
+#y_t = testdata[['realSum']].values
+#predictions = model.predict(x_t)
+#model.save("/mnt/c/Users/Admin/FOM-Anwendungsprojekt/Models/c_rt-g_s_o-as_n-100epochs_002.h5")
