286b1355283315d777dfab5ae7448f1c6a458acd
Marian <101745797+KesperM@users.noreply.github.com>
2024-01-17 19:46:39 +0100

    Update Tensorflow_load_01.py

--- Methoden/Tensorflow_load_01.py
+++ Methoden/Tensorflow_load_01.py
@@ -6,9 +6,12 @@ Created on Wed Jan 17 18:50:12 2024
 """
 
 from tensorflow import keras
+import pandas as pd
 
+df = pd.read_csv('C:/DATA/Documents/GitHub/FOM-Anwendungsprojekt/Data/Output/Airbnb_Prices_Full_V1.0_Train.csv', sep=';', decimal='.')
+x = df[['attr_index_norm', 'rest_index_norm', 'person_capacity', 'AttractionScore_Norm']].values
 # Pfad zur H5-Datei
-model_path = 'C:/DATA/Documents/GitHub/FOM-Anwendungsprojekt/Models/train-csv-a_i_n-r_i_n-p_c-AS_N-100epochs_001.h5'
+model_path = 'C:/DATA/Documents/GitHub/FOM-Anwendungsprojekt/Models/train-csv-a_i_n-r_i_n-p_c-AS_N-1000epochs_001.h5'
 
 # Laden des Modells
 model = keras.models.load_model(model_path)
@@ -17,7 +20,7 @@ model = keras.models.load_model(model_path)
 model.summary()
 
 # Angenommen, `some_input_data` ist Ihre Eingabedaten
-predictions = model.predict(some_input_data)
+predictions = model.predict(df)
 
 # Ergebnisse anzeigen oder weiterverarbeiten
 print(predictions)
