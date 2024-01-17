68885f9958efa58e95fd1d024ebe07694f448d77
Marian <101745797+KesperM@users.noreply.github.com>
2024-01-17 20:05:41 +0100

    Update Tensorflow_load_02.py

--- Methoden/Tensorflow_load_02.py
+++ Methoden/Tensorflow_load_02.py
@@ -8,10 +8,10 @@ Created on Wed Jan 17 19:57:44 2024
 from tensorflow import keras
 import pandas as pd
 
-df = pd.read_csv('C:/Users/MK/FOM-Anwendungsprojekt/Data/Output/Airbnb_Prices_V1.0_Test.csv', sep=';', decimal='.')
+df = pd.read_csv('/mnt/c/Users/MK/FOM-Anwendungsprojekt/Data/Output/Airbnb_Prices_V1.0_Test.csv', sep=';', decimal='.')
 x = df[['attr_index_norm', 'rest_index_norm', 'person_capacity', 'AttractionScore_Norm']].values
 # Pfad zur H5-Datei
-model_path = 'C:/Users/MK/FOM-Anwendungsprojekt/Models/train-csv-a_i_n-r_i_n-p_c-AS_N-1000epochs_001.h5'
+model_path = '/mnt/c/Users/MK/FOM-Anwendungsprojekt/Models/train-csv-a_i_n-r_i_n-p_c-AS_N-1000epochs_001.h5'
 
 # Laden des Modells
 model = keras.models.load_model(model_path)
