6f2f1534e56e80f9d054ac3391d729417db7988f
Marian <101745797+KesperM@users.noreply.github.com>
2024-01-17 19:23:53 +0100

    Update Tensorflow_load_01.py

--- Methoden/Tensorflow_load_01.py
+++ Methoden/Tensorflow_load_01.py
@@ -8,10 +8,16 @@ Created on Wed Jan 17 18:50:12 2024
 from tensorflow import keras
 
 # Pfad zur H5-Datei
-model_path = '"C:/DATA/Documents/GitHub/FOM-Anwendungsprojekt/Models/train-csv-a_i_n-r_i_n-p_c-AS_N-100epochs_001.h5".h5'
+model_path = 'C:/DATA/Documents/GitHub/FOM-Anwendungsprojekt/Models/train-csv-a_i_n-r_i_n-p_c-AS_N-100epochs_001.h5'
 
 # Laden des Modells
 model = keras.models.load_model(model_path)
 
 # Modellübersicht anzeigen
 model.summary()
+
+# Angenommen, `some_input_data` ist Ihre Eingabedaten
+predictions = model.predict(some_input_data)
+
+# Ergebnisse anzeigen oder weiterverarbeiten
+print(predictions)
