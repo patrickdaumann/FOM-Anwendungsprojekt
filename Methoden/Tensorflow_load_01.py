3012974942f1700fb6d07a0482f67499224c3408
Marian <101745797+KesperM@users.noreply.github.com>
2024-01-17 18:53:21 +0100

    Create Tensorflow_load_01.py

---
+++ Methoden/Tensorflow_load_01.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 17 18:50:12 2024
+
+@author: kesper
+"""
+
+from tensorflow import keras
+
+# Pfad zur H5-Datei
+model_path = '"C:/DATA/Documents/GitHub/FOM-Anwendungsprojekt/Models/train-csv-a_i_n-r_i_n-p_c-AS_N-100epochs_001.h5".h5'
+
+# Laden des Modells
+model = keras.models.load_model(model_path)
+
+# Modellübersicht anzeigen
+model.summary()
