06a49d2e49923cbef7a9a285a5e4ec8a62b37441
Marian <101745797+KesperM@users.noreply.github.com>
2024-01-17 20:26:41 +0100

    Update Tensorflow_load_02.py

--- Methoden/Tensorflow_load_02.py
+++ Methoden/Tensorflow_load_02.py
@@ -10,6 +10,8 @@ import pandas as pd
 
 df = pd.read_csv('/mnt/c/Users/MK/FOM-Anwendungsprojekt/Data/Output/Airbnb_Prices_V1.0_Test.csv', sep=';', decimal='.')
 x = df[['attr_index_norm', 'rest_index_norm', 'person_capacity', 'AttractionScore_Norm']].values
+#x = x.astype('float32')  # Konvertieren in float32
+
 # Pfad zur H5-Datei
 model_path = '/mnt/c/Users/MK/FOM-Anwendungsprojekt/Models/train-csv-a_i_n-r_i_n-p_c-AS_N-1000epochs_001.h5'
 
@@ -20,7 +22,9 @@ model = keras.models.load_model(model_path)
 model.summary()
 
 # Angenommen, `some_input_data` ist Ihre Eingabedaten
-predictions = model.predict(df)
+predictions = model.predict(x)
 
 # Ergebnisse anzeigen oder weiterverarbeiten
 print(predictions)
+
+#test
