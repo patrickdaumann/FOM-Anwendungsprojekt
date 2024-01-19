5c038a47808b452135099e284c934e372fe9c9c5
Marian <101745797+KesperM@users.noreply.github.com>
2024-01-19 10:04:14 +0100

    New Model

--- Methoden/Tensorflow_04_linux.py
+++ Methoden/Tensorflow_04_linux.py
@@ -32,7 +32,7 @@ model = keras.Sequential([
 model.compile(optimizer='adam', loss='mean_squared_error')  # Für Regressionsaufgaben
  
 # Training des Modells
-model.fit(x, y, epochs=10000)  # Wir verwenden die gleichen Daten für Training und Test
+model.fit(x, y, epochs=15000)  # Wir verwenden die gleichen Daten für Training und Test
  
 # Evaluieren des Modells (optional)
 loss = model.evaluate(x, y)
@@ -41,7 +41,7 @@ print(f'Loss auf den Trainingsdaten: {loss:.4f}')
 # Vorhersagen mit dem trainierten Modell
 predictions = model.predict(x)
 print(predictions)
-model.save("/mnt/c/Users/Admin/FOM-Anwendungsprojekt/Models/train-csv-r_t_e-r_i_n-m_d-d-b-AS_N-c_e-10kepochs_001.h5")
+model.save("/mnt/c/Users/Admin/FOM-Anwendungsprojekt/Models/train-csv-r_t_e-r_i_n-m_d-d-b-AS_N-c_e-15kepochs_001.h5")
  
 # Die Vorhersagen sollten nun nahe an den Zielvariablen liegen, da es sich um ein einfaches Beispiel handelt.
 
